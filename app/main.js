e25bda19f62444b21c558abd95a5035c5040328c
Tae <willtk0113@gmail.com>
2019-03-31 12:32:50 +0900

    First Commit

---
+++ app/main.js
@@ -0,0 +1,18 @@
+const {app, BrowserWindow } = require('electron');
+
+let mainWindow = null;
+
+app.on('ready', () => {
+    mainWindow = new BrowserWindow({
+        show : false,
+    });
+
+    mainWindow.loadFile(`${__dirname}\\index.html`);
+    mainWindow.once('ready-to-show', () =>{
+        mainWindow.show();
+        //mainWindow.webContents.openDevTools();
+    })
+    mainWindow.on('closed', () =>{
+        mainWindow = null;
+    });
+});
